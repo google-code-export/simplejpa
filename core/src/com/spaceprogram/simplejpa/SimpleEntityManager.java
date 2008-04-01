9adfe0e0c2f317f7adb5c31423d60d2f6d055e0b
treeder <treeder@195de80e-6d41-0410-879a-d7ca2a8a07e1>
2008-04-01 21:14:48 +0000

    Added new SimpleEntityManager interface with async persists.

---
+++ core/src/com/spaceprogram/simplejpa/SimpleEntityManager.java
@@ -0,0 +1,35 @@
+package com.spaceprogram.simplejpa;
+
+import javax.persistence.EntityManager;
+import java.util.concurrent.Future;
+
+/**
+ * This is an extended EntityManager interface to add extra functionality and performance enhancements specific
+ * to databases like SimpleDB.
+ *
+ * User: treeder
+ * Date: Apr 1, 2008
+ * Time: 11:42:43 AM
+ */
+public interface SimpleEntityManager extends EntityManager {
+
+    /**
+     * This is the same as persist(Object), but will return immediately and do the actual
+     * save to the database in the background.
+     *
+     * You can use the Future that is returned to make see if any exceptions occurred while doing the save by
+     * calling Future.get() and wrapping that in a try/catch. If an Exception occurred, you will get an
+     * ExecutionException and you can call getCause() on that to get the original exception.
+     * ie:
+     * try {
+     *    future.get(); // this will block until the task has completed.
+     * } catch (ExecutionException ex){
+     *    ex.getCause().printStackTrace();
+     * }
+     *
+     * @param o
+     * @return
+     */
+    public Future persistAsync(Object o);
+
+}
