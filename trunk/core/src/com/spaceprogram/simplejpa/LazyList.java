b24c636710931dfbd30945f777541180ac61f0d4
treeder <treeder@195de80e-6d41-0410-879a-d7ca2a8a07e1>
2010-02-05 18:10:47 +0000

    * Swapped out with reformy's version that fixes no results with next token.

--- trunk/core/src/com/spaceprogram/simplejpa/LazyList.java
+++ trunk/core/src/com/spaceprogram/simplejpa/LazyList.java
@@ -10,10 +10,7 @@ import org.apache.commons.collections.list.GrowthList;
 
 import javax.persistence.PersistenceException;
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,45 +18,50 @@ import java.util.logging.Logger;
  * Loads objects in the list on demand from SimpleDB.
  * <p/>
  * <p/>
- * User: treeder
- * Date: Feb 10, 2008
- * Time: 9:06:16 PM
+ * User: treeder Date: Feb 10, 2008 Time: 9:06:16 PM
  */
-public class LazyList extends AbstractList implements Serializable {
+@SuppressWarnings("unchecked")
+public class LazyList<E> extends AbstractList<E> implements Serializable {
+    private static final int MAX_RESULTS_PER_REQUEST = 2500;
+
     private static Logger logger = Logger.getLogger(LazyList.class.getName());
+
     private transient EntityManagerSimpleJPA em;
     private Class genericReturnType;
-    private QueryImpl query;
-    //    private List<Object> items = new ArrayList();
+    private QueryImpl origQuery;
+
     /**
      * Stores the actual objects for this list
      */
-    private List backingList = new GrowthList();
-    //    private int numRetrieved = 0;
+    private List<E> backingList;
     private String nextToken;
-    private int maxToRetrievePerRequest = 100; // same as amazon default
-    private int numPagesLoaded;
-    private Long count;
-    //    private transient Map<String, Future<ItemAndAttributes>> futuresMap = new ConcurrentHashMap();
-    /**
-     * map to remember which pages have been loaded already.
-     */
-    private Map<Integer, Integer> materializedPages = new HashMap<Integer, Integer>();
-    /**
-     * Max results based on the query sent in.
-     * todo: implement using this
-     */
-    private Integer maxResults;
-
-    public LazyList() {
-        super();
-    }
+    private int count = -1;
+    private String realQuery;
+    private Domain domain;
+    private int maxResults = -1;
 
     public LazyList(EntityManagerSimpleJPA em, Class tClass, QueryImpl query) {
-        this();
+        this(em, tClass, query, -1);
+    }
+
+    public LazyList(EntityManagerSimpleJPA em, Class tClass, QueryImpl query, int maxResults) {
         this.em = em;
         this.genericReturnType = tClass;
-        this.query = query;
+        this.origQuery = query;
+        AnnotationInfo ai = em.getAnnotationManager().getAnnotationInfo(genericReturnType);
+        try {
+            domain = em.getDomain(ai.getRootClass());
+            if (domain == null) {
+                logger.warning("Domain does not exist for " + ai.getRootClass());
+                backingList = new GrowthList(0);
+            } else {
+                realQuery = query.createAmazonQuery().getValue();
+                this.maxResults = maxResults;
+            }
+        }
+        catch (Exception e) {
+            throw new PersistenceException(e);
+        }
     }
 
     public boolean isEmpty() {
@@ -67,156 +69,140 @@ public class LazyList extends AbstractList implements Serializable {
     }
 
     public int size() {
-//        System.out.println("SIZE called");
-        if(numPagesLoaded == 0) get(0); // make sure it's loaded first page
-        if(nextToken == null){
-//            System.out.println("no next token, so size == backinglist.size" + backingList.size());
-            return backingList.size();
-        }
-        // todo: do a quick precheck if no next token or something, then no need to call this count
-        if (count != null) return count.intValue();
-        try {
-            JPAQuery queryClone = (JPAQuery) query.getQ().clone();
-            queryClone.setResult("count(*)");
-            QueryImpl query2 = new QueryImpl(em, queryClone);
-            query2.setParameters(query.getParameters());
-            query2.setForeignIds(query.getForeignIds());
-//            System.out.println("query for size=" + query2.toString());
-            List results = query2.getResultList();
-//            System.out.println("obs.size=" + results.size());
-            count = (Long) results.get(0);
-//            System.out.println("count set to " + count);
+        if (count > -1)
+            return count;
 
-        } catch (CloneNotSupportedException e) {
-            throw new RuntimeException(e);
+        if (backingList != null && nextToken == null) {
+            count = backingList.size();
+        } else {
+            try {
+                if (logger.isLoggable(Level.FINER))
+                    logger.finer("Getting size.");
+                JPAQuery queryClone = (JPAQuery) origQuery.getQ().clone();
+                queryClone.setResult("count(*)");
+                QueryImpl query2 = new QueryImpl(em, queryClone);
+                query2.setParameters(origQuery.getParameters());
+                query2.setForeignIds(origQuery.getForeignIds());
+                List results = query2.getResultList();
+                int resultCount = ((Long) results.get(0)).intValue();
+                if (logger.isLoggable(Level.FINER))
+                    logger.finer("Got:" + resultCount);
+
+                if (maxResults > -1 && resultCount > maxResults) {
+                    if (logger.isLoggable(Level.FINER))
+                        logger.finer("Too much, adjusting to maxResults: " +
+                                maxResults);
+                    count = maxResults;
+                } else {
+                    count = resultCount;
+                }
+            }
+            catch (CloneNotSupportedException e) {
+                throw new PersistenceException(e);
+            }
         }
-//        loadAllItems();
-        return count.intValue();
+        return count;
     }
 
-    public void add(int index, Object element) {
+    public void add(int index, E element) {
         backingList.add(index, element);
     }
 
-    public Object set(int index, Object element) {
+    public E set(int index, E element) {
         return backingList.set(index, element);
     }
 
-    public Object remove(int index) {
+    public E remove(int index) {
         return backingList.remove(index);
     }
 
-    public static void main(String[] args) {
-        int x = 32 / 100;
-        System.out.println("x=" + x);
+    public E get(int i) {
+        if (logger.isLoggable(Level.FINER))
+            logger.finer("getting from lazy list at index=" + i);
+        loadAtleastItems(i);
+        return backingList.get(i);
     }
 
-    public Object get(int i) {
-        if (logger.isLoggable(Level.FINER)) logger.finer("getting from lazy list at index=" + i);
-        loadItems(i / maxToRetrievePerRequest, true);
-        Object o = null;
-        if (backingList.size() > i) {
-            o = backingList.get(i);
-            if (o != null) {
-                logger.finest("object already loaded in backing list: " + o);
-                return o;
-            }
-        }
-        return o;
-    }
-
-    private Object checkCache(SdbItem item) {
-        Object o = em.cacheGet(genericReturnType, item.getIdentifier());
-        return o;
-    }
-
-    private void putInBackingList(int i, Object o) {
-        backingList.set(i, o);
-    }
-
-    private synchronized void loadItems(int page, boolean materializeObjectsInPage) {
-        if (numPagesLoaded > page) {
-            if (materializeObjectsInPage) materializeObjectsInPage(page);
+    private synchronized void loadAtleastItems(int index) {
+        if (backingList != null && nextToken == null) {
             return;
         }
-        if (numPagesLoaded > 0 && nextToken == null) {
-            // no more results to get
-            if (materializeObjectsInPage) materializeObjectsInPage(page);
-            return;
-        }
-//        System.out.println("loadItems page=" + page);
 
-        Domain domain;
-        try {
-            AnnotationInfo ai = em.getAnnotationManager().getAnnotationInfo(genericReturnType);
-            domain = em.getDomain(ai.getRootClass());
-            if (domain == null) {
-                logger.warning("Domain does not exist for " + ai.getRootClass());
-                numPagesLoaded = 1;
-                return;
-            }
-            // Now we'll load up all the items up to the page specified
-            for (int i = numPagesLoaded; i <= page; i++) {
-                if (logger.isLoggable(Level.FINER)) logger.finer("loading items for list. Page=" + i);
-                if (numPagesLoaded > 0 && nextToken == null) {
+        if (backingList == null) {
+            backingList = new GrowthList();
+        }
+
+        while (backingList.size() <= index) {
+            QueryWithAttributesResult qr;
+            try {
+                if (logger.isLoggable(Level.FINER))
+                    logger.finer("query for lazylist=" + origQuery);
+
+                int limit = maxResults - backingList.size();
+                String limitQuery = realQuery
+                        + " limit "
+                        + (noLimit() ? MAX_RESULTS_PER_REQUEST :
+                        (limit > MAX_RESULTS_PER_REQUEST ? MAX_RESULTS_PER_REQUEST
+                                : limit));
+                if (em.getFactory().isPrintQueries())
+                    System.out.println("query in lazylist=" + limitQuery);
+                qr = domain.selectItems(limitQuery, nextToken);
+
+                Map<String, List<ItemAttribute>> itemMap = qr.getItems();
+                if (logger.isLoggable(Level.FINER))
+                    logger.finer("got items for lazylist=" + itemMap.size());
+                for (String id : itemMap.keySet()) {
+                    List<ItemAttribute> list = itemMap.get(id);
+                    backingList.add((E) em.buildObject(genericReturnType, id, list));
+                }
+
+                if (qr.getNextToken() == null || (!noLimit() && qr.getItems().size() == limit)) {
+                    nextToken = null;
                     break;
                 }
-                QueryWithAttributesResult qr;
-                try {
-                    if (logger.isLoggable(Level.FINER)) logger.finer("query for lazylist=" + query);
-                    if (em.getFactory().isPrintQueries()) System.out.println("query in lazylist=" + query);
-                    qr = domain.selectItems(query.createAmazonQuery().getValue(), nextToken); // todo: maxToRetrievePerRequest, need to use limit now
-                    Map<String, List<ItemAttribute>> itemMap = qr.getItems();
-//                    List<Item> itemList = qr.getResultList();
-                    if (logger.isLoggable(Level.FINER)) logger.finer("got items for lazylist=" + itemMap.size());
-                    // dang, this new select thing in typica sucks, why would the list of results be a map?
-                    for (String id : itemMap.keySet()) {
-                        List<ItemAttribute> list = itemMap.get(id);
-                        backingList.add(em.buildObject(genericReturnType, id, list));
-                    }
 
-//                    items.addAll(itemList);
-//                    numRetrieved += itemMap.size();
-                    nextToken = qr.getNextToken();
-                    numPagesLoaded++;
-                    if (materializeObjectsInPage) {
-                        materializeObjectsInPage(page);
-                    }
-                } catch (SDBException e) {
-                    if (ExceptionHelper.isDomainDoesNotExist(e)) {
-//                        items = new ArrayList(); // no need to throw here
-                    } else {
-                        throw new PersistenceException("Query failed: Domain=" + domain.getName() + " -> " + query, e);
-                    }
+                if (!noLimit() && qr.getItems().size() > limit) {
+                    throw new PersistenceException("Got more results than the limit.");
                 }
-//                logger.finer("got " + items.size() + " for lazy list");
+
+                nextToken = qr.getNextToken();
             }
-        } catch (SDBException e) {
-            throw new PersistenceException(e);
+            catch (SDBException e) {
+                throw new PersistenceException("Query failed: Domain="
+                        + domain.getName() + " -> " + origQuery, e);
+            }
         }
-    }
-
-    private void materializeObjectsInPage(int page) {
-        if (materializedPages.get(page) != null) {
-            return;
-        }
-        materializedPages.put(page, page);
 
     }
 
+    private boolean noLimit() {
+        return maxResults == -1;
 
-    private void loadAllItems() {
-//        System.out.println("loadAllItems");
-        int page = 0;
-        while (nextToken != null || numPagesLoaded == 0) {
-            loadItems(page, false);
-            page++;
-        }
     }
 
+    @Override
+    public Iterator<E> iterator() {
+        return new LazyListIterator();
+    }
 
     public void setMaxResults(Integer maxResults) {
-        this.maxResults = maxResults;
+        // this.maxResults = maxResults;
     }
 
+    private class LazyListIterator implements Iterator<E> {
+        private int iNext = 0;
+
+        public boolean hasNext() {
+            loadAtleastItems(iNext);
+            return backingList.size() > iNext;
+        }
+
+        public E next() {
+            return get(iNext++);
+        }
+
+        public void remove() {
+            LazyList.this.remove(iNext - 1);
+        }
+    }
 }
